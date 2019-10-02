a29c4ddbee09fa4f3869b4c1092c5461454786b9
VladimirFedoruk <anvil.prog@gmail.com>
2019-10-02 23:56:06 +0300

    repeated statement if

---
+++ TypeCasting/src/IfStairs.java
@@ -0,0 +1,16 @@
+public class IfStairs {
+    public static void main(String[] args) {
+        for (int x = 0; x < 4; x++){
+            if (x == 1)
+                System.out.println("один");
+            else if (x == 2)
+                System.out.println("два");
+            else if (x == 3)
+                System.out.println("три");
+            else
+                System.out.println("ноль");
+
+        }
+
+    }
+}
